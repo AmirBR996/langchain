a859c0578c01864481cf742301705cfd7582f38c
Amir <amirgulmeli123@gmail.com>
2026-02-24 19:43:44 +0545

    lambda

--- 6.Runnables/runnable_lambda.py
+++ 6.Runnables/runnable_lambda.py
@@ -1,1 +1,7 @@
-    
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence
+load_dotenv()
+       
